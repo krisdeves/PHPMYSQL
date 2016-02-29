fbbe7666c170657b29efbb8fc5e92da0f5fdfa71
TristanPinaudeau <tristan.pinaudeau@epsi.fr>
2016-02-29 14:26:55 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="general/style_general.css"/>
+    </head>
+    
+    <body>
+
+        <article id="Bandeau_titre">
+            <a href="index.php"><img id="logo" src="general/logo.jpg"/></a>
+            <h1>CompFundation</h1>
+            <h2>L'entreprise des services numériques</h2>
+        </article>
+
+        <nav>
+            <ul id="Bandeau_menu">
+
+                <li class="menu">
+                    <a href="index.php">ACCUEIL</a>
+                </li>
+                
+                <li class="menu">
+                    <a href="#">INSCRIPTION</a>
+                    
+                    <ul id="sous-menu">
+                        <li>
+                            <a href="inscription/inscription_int.php">Intervenant</a>
+                        </li>
+                        <li>
+                            <a href="inscription/inscription_org.php">Organisme</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="menu">
+                    <a href="recherche/">RECHERCHE</a>
+                </li>
+                
+                <li class="menu">
+                    <a href="contact.php">CONTACT</a>
+                </li>
+
+            </ul>
+        </nav>
+
+        <div id="corp">
+            <br/>
+
+            F I C H E    D E    C O N T A C T
+            
+            <br/>
+        </div>
+
+        <footer>
+            <a href="index.php">@CompFundation</a>
+            <a href="#">Mention légales</a>
+        </footer>
+
+    </body>
+    
+</html>
